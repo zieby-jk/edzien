a9ee9ba7437147bdda99a079fd45c3eee17b0617
TymianekPL <tymi@tymi.org>
2025-09-18 13:06:08 +0200

    express setup

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-console.log("Meow!");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const server = express().listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}!`);
+});
