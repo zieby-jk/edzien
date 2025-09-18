7696f086d9d7b9195a511019a96eadb63e83eb4f
Tymianek <tymi@tymi.org>
2025-09-18 17:28:25 +0200

    Update eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,7 +5,27 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      "semi": ["error", "always"],                  // Enforce semicolons
+      "quotes": ["error", "double"],                // Enforce double quotes
+      "indent": ["error", 5],                       // Enforce 5 spaces indentation
+    }
+  },
   tseslint.configs.recommended,
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] }
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"]
+  }
 ]);
