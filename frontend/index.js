b72a124c81cb7042fb2e71e4a9a9c642d9eff2a8
Jacob989801 <kuba.jelonkiewicz@gmail.com>
2025-09-21 16:51:44 +0200

    fixing dots and creating menu

--- frontend/index.js
+++ frontend/index.js
@@ -1,22 +1,24 @@
 
 let account = document.querySelector(".account")
-let accuntDefaultIcon = document.querySelector(".icon")
-let accuntActiveIcon = document.querySelector(".active-icon")
+let accountDefaultIcon = document.querySelector(".icon")
+let accountActiveIcon = document.querySelector(".active-icon")
 let heading = document.querySelector(".heading") 
+let header = document.querySelector("header")
 let registerDiv = document.querySelector(".register")
 let registerLink = document.querySelector(".register-link")
-console.log(account)
+let isAccountOptions = false;
 account.onmouseover = () =>{
-    accuntDefaultIcon.style.opacity = 0;
-    accuntActiveIcon.style.display = "block";
-    accuntActiveIcon.style.opacity = 1;
+    accountDefaultIcon.style.opacity = 0;
+    CreatingOptions()
+    accountActiveIcon.style.opacity = 1;
     
 }
 account.onmouseleave= () =>{
-    accuntDefaultIcon.style.opacity = 1;
-    accuntActiveIcon.style.opacity = 0;
+    accountDefaultIcon.style.opacity = 1;
+    accountActiveIcon.style.opacity = 0;
     
 }
+
 window.onload = () =>{
     Loading();
 }
@@ -41,14 +43,14 @@ function Loading() {
         }, 100);
 
         
-        if (dots > 3) {
+        if (dots == 3) {
             els.forEach(el=>{
                 setTimeout(()=>{
                     el.style.opacity = "0";
                     setTimeout(() => {
                         heading.removeChild(el);
                     }, 300);
-                }, 300);
+                }, 500);
             });
             dots = 0;
             els = [];
@@ -62,4 +64,31 @@ registerDiv.onmouseover = () =>{
 }
 registerDiv.onmouseleave = () => {
     registerLink.style.color = "#2854C5"
+}
+function CreatingOptions(){
+    if(isAccountOptions){
+        return;
+    }
+    isAccountOptions = true
+    let options = 
+    [
+        {className: "account", htmlTag: "div"},
+        {className: "settings", htmlTag: "div"},
+        {className: "help", htmlTag: "div"},
+    ]
+    let option;
+    
+    let optionsContainer = document.createElement("div")
+    optionsContainer.style.opacity = "0";
+    setTimeout(()=>{
+        optionsContainer.style.opacity = "1"
+    }, 50)
+    header.appendChild(optionsContainer)
+    optionsContainer.classList.add("account-options")
+    options.forEach(o=>{
+        option = document.createElement(o.htmlTag);
+        option.classList.add(o.className)
+        optionsContainer.appendChild(option)
+        console.log(option)
+    })
 }
