2774ce15dd6d3927ab89d6b5c1ff059b73580a11
TymianekPL <tymi@tymi.org>
2025-10-09 13:03:08 +0200

    eslint (10 errors remaining)

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -13,34 +13,34 @@ let labels = document.querySelectorAll(".login-form-label");
 
 let isInputClicked = false;
 inputs.forEach(input => {
-    input.onmouseover = () => {
-        InputOnHover(input);
-    }
-    input.onfocus = () => {
-        InputClicked(input);
-    }
-    input.onblur = () => {
-        isInputClicked = false
-        input.classList.toggle("active")
+     input.onmouseover = () => {
+          InputOnHover(input);
+     };
+     input.onfocus = () => {
+          InputClicked(input);
+     };
+     input.onblur = () => {
+          isInputClicked = false;
+          input.classList.toggle("active");
 
-        console.log("odkliknięto")
+          console.log("odkliknięto");
 
-    }
-})
+     };
+});
 function InputClicked(clickedInput) {
-    if (isInputClicked) return;
-    isInputClicked = true
-    console.log("kliknięto")
-    let inputBorder = document.createElement("span")
-    clickedInput.appendChild(inputBorder)
-    setTimeout(() => {
-        inputBorder.classList.add("input-border")
-    })
+     if (isInputClicked) return;
+     isInputClicked = true;
+     console.log("kliknięto");
+     let inputBorder = document.createElement("span");
+     clickedInput.appendChild(inputBorder);
+     setTimeout(() => {
+          inputBorder.classList.add("input-border");
+     });
 }
 
 
 
 function InputOnHover(hoveredInput) {
-    let hoveredLabel = document.querySelector(`.login-form-label[for=${hoveredInput.name}]`)
-    console.log(hoveredLabel)
+     let hoveredLabel = document.querySelector(`.login-form-label[for=${hoveredInput.name}]`);
+     console.log(hoveredLabel);
 }
