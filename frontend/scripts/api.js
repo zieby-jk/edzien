f41b8727644b0fb56f7988bd6d5ed42d90ed94df
TymianekPL <tymi@tymi.org>
2025-10-10 10:34:46 +0200

    More fetch methods and a sample API endpoint in frontend

--- frontend/scripts/api.js
+++ frontend/scripts/api.js
@@ -15,7 +15,37 @@ const API = {
                     Authorization: API.accessToken
                }
           });
+     },
+     AuthPost: async (url, data) => {
+          if (API.accessToken === null) {
+               throw new Error("Access token is null");
+          }
+          return await fetch(url, {
+               headers: {
+                    "Content-Type": "aplication/json",
+                    Authorization: API.accessToken
+               },
+               method: "POST",
+               body: JSON.stringify(data)
+          });
+     },
+     AuthDelete: async (url) => {
+          if (API.accessToken === null) {
+               throw new Error("Access token is null");
+          }
+          return await fetch(url, {
+               headers: {
+                    "Content-Type": "aplication/json",
+                    Authorization: API.accessToken
+               },
+               method: "DELETE"
+          });
+     },
+     schools: {
+          _cache: [],
+          refresh: async () => {
+               _cache = await fetch("/api/school/lists").then(r => r.json());
+          },
+          get list() { return this._cache; }
      }
-
-
 };
