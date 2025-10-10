6ef768b1733f559ea5150948bc0285db1e4a586f
Jacob989801 <125312628+Jacob989801@users.noreply.github.com>
2025-10-10 09:55:46 +0200

    User/kuba/creating main page (#6)
    * First prototype of mainpage, many things to add and responsibility to repair (not working)
    
    * fixing dots and creating menu
    
    * small fixes
    
    * fixing menu issues
    
    * adding login page, very begining
    
    * adding api
    
    * addig api and login
    
    * eslint (10 errors remaining)
    
    * fixing small problems
    
    ---------
    
    Co-authored-by: --unset <--unset>
    Co-authored-by: TymianekPL <tymi@tymi.org>

---
+++ frontend/scripts/index.js
@@ -0,0 +1,146 @@
+
+let account = document.querySelector(".account");
+let accountDefaultIcon = document.querySelector(".icon");
+let accountActiveIcon = document.querySelector(".active-icon");
+let heading = document.querySelector(".heading");
+let header = document.querySelector("header");
+let registerDiv = document.querySelector(".register");
+let registerLink = document.querySelector(".register-link");
+let isAccountOptions = false;
+let isAccountOptionsClicked = false;
+let optionsContainer;
+
+account.onmouseover = () => {
+     accountDefaultIcon.style.opacity = 0;
+     CreatingOptions();
+     CleanCounting();
+     accountActiveIcon.style.opacity = 1;
+
+
+};
+let interval;
+let leavingTime;
+account.onmouseleave = async () => {
+     accountDefaultIcon.style.opacity = 1;
+     accountActiveIcon.style.opacity = 0;
+     RemoveAccountOptions();
+};
+window.onload = () => {
+     Loading();
+};
+function CleanCounting() {
+     if (interval != null) {
+          clearInterval(interval);
+     }
+}
+async function RemoveAccountOptions() {
+     leavingTime = 0;
+     CleanCounting();
+
+     interval = setInterval(() => {
+          leavingTime++;
+          console.log(leavingTime);
+
+     }, 1000);
+     await new Promise(time => setTimeout(time, 1000));
+
+
+     if (isAccountOptions && !isAccountOptionsClicked && leavingTime >= 1) {
+          optionsContainer.style.transition = "opacity 0.5s ease";
+          optionsContainer.style.opacity = "0";
+          setTimeout(() => {
+               optionsContainer.remove();
+               CleanCounting();
+          }, 500);
+
+          isAccountOptions = false;
+     }
+}
+function Loading() {
+     let dots = 0;
+     let els = [];
+     setInterval(() => {
+          dots++;
+          let el = document.createElement("p");
+          el.style.display = "inline";
+          el.style.gap = "small";
+          el.textContent = ".";
+          el.style.transition = ".3s ease-in";
+          heading.appendChild(el);
+          els.push(el);
+
+
+          el.style.opacity = "0";
+          setTimeout(() => {
+               el.style.opacity = "1";
+          }, 100);
+
+
+          if (dots == 3) {
+               els.forEach(el => {
+                    setTimeout(() => {
+                         el.style.opacity = "0";
+                         setTimeout(() => {
+                              heading.removeChild(el);
+                         }, 300);
+                    }, 500);
+               });
+               dots = 0;
+               els = [];
+          }
+     }, 900);
+}
+
+registerDiv.onmouseover = () => {
+     registerLink.style.color = "#00ffff";
+
+};
+registerDiv.onmouseleave = () => {
+     registerLink.style.color = "#2854C5";
+};
+function CreatingOptions() {
+     if (isAccountOptions) {
+          return;
+     }
+     isAccountOptions = true;
+     let options =
+        [
+             { className: "account-info", htmlTag: "div", delayTime: 300 },
+             { className: "settings", htmlTag: "div", delayTime: 600 },
+             { className: "help", htmlTag: "div", delayTime: 900 }
+        ];
+
+
+     optionsContainer = document.createElement("div");
+     optionsContainer.style.opacity = "0";
+     setTimeout(() => {
+          optionsContainer.style.opacity = "1";
+     }, 50);
+     header.appendChild(optionsContainer);
+     optionsContainer.classList.add("account-options");
+     let x = 0;
+     options.forEach(o => {
+          x++;
+          let option = document.createElement(o.htmlTag);
+          optionsContainer.appendChild(option);
+          option.style.opacity = "0";
+          setTimeout(() => {
+               option.style.opacity = "1";
+          }, o.delayTime);
+          option.classList.add(o.className);
+          option.classList.add("account-option");
+          option.textContent = `opcja ${x}`;
+
+          if (optionsContainer) {
+               Array.from(optionsContainer.children).forEach(child => {
+                    child.onmouseover = () => {
+                         CleanCounting();
+                    };
+                    child.onmouseleave = RemoveAccountOptions;
+               });
+          }
+
+
+     });
+
+}
---
+++ frontend/scripts/login.js
@@ -0,0 +1,46 @@
+// let form = document.querySelector(".login-form");
+let inputs = document.querySelectorAll(".login-form-input");
+// let labels = document.querySelectorAll(".login-form-label");
+// labels.forEach(label =>{
+//     label.onclick = () =>{ 
+//         label.style.display = "none";
+//         let inputName = label.htmlFor
+//         let input = document.querySelector(`input[name=${inputName}]`)
+//         input.classList.toggle("focus")
+//         InputClicked(input)
+//      }
+// })
+
+let isInputClicked = false;
+inputs.forEach(input => {
+     input.onmouseover = () => {
+          InputOnHover(input);
+     };
+     input.onfocus = () => {
+          InputClicked(input);
+     };
+     input.onblur = () => {
+          isInputClicked = false;
+          input.classList.toggle("active");
+
+          console.log("odkliknięto");
+
+     };
+});
+function InputClicked(clickedInput) {
+     if (isInputClicked) return;
+     isInputClicked = true;
+     console.log("kliknięto");
+     let inputBorder = document.createElement("span");
+     clickedInput.appendChild(inputBorder);
+     setTimeout(() => {
+          inputBorder.classList.add("input-border");
+     });
+}
+
+
+
+function InputOnHover(hoveredInput) {
+     let hoveredLabel = document.querySelector(`.login-form-label[for=${hoveredInput.name}]`);
+     console.log(hoveredLabel);
+}
